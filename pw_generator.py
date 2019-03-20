45c8494f0b4eff96daa9778a07bd2a3320683db1
Brian Fong <bwfong@sfu.ca>
2019-03-20 13:07:45 -0700

    Added general outline for logical execution Added description for pw_generator

--- pw_generator.py
+++ pw_generator.py
@@ -8,24 +8,38 @@ class pw_generator:
     """
     A password generator
     """
-    def __init__(self):
+    def __init__(self, user = str()):
         """
+        Initialize variables needed 
+        Receive user input 
+        Generate password and print
+        Save questions asked as a txt file
+        """
+        self.lower_char = 'qwertyuiopasdfghjklzxcvbnm'
+        self.upper_char = self.lower_char.upper()
+        self.nums = '1234567890'
+        self.symbols = '!@#$%^&*()-=_+[]:;",.<>/?`~'
+        self.invalid_symbols = str()
+        self.answer = list()
+        self.seed = int()
+        self.valid_char = str()
+        self.password_len = int()
+        self.password = str()
+        self.user = str()
 
-        """
-        lower_char = 'qwertyuiopasdfghjklzxcvbnm'
-        upper_char = lower_char.upper()
-        nums = '1234567890'
-        symbols = '!@#$%^&*()-=_+[]:;",.<>/?`~'
-        invalid_symbols = str()
-        answer = list()
-        seed = int()
-        password = str()
+        self.read_file_as_bytes()
+        self.ask_question()
+        self.generate_seed()
+        self.scramble()
+        self.substitute()
+        print(self.password)
         return
 
     def read_file_as_bytes(self):
         """
 
         """
+
         return
     
     def write_file_from_bytes(self):
