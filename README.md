7f0fb4d88e05c7e5af4e0b246cdb39ebdbca8cd5
Brian Fong <bwfong@sfu.ca>
2019-03-20 12:57:06 -0700

    Initial pw_generator script  added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Password-Generator
+A repository for python scripts to generate passwords based on questions answered
---
+++ pw_generator.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import random
+import math
+import os
+import argparse
+
+class pw_generator:
+    """
+    A password generator
+    """
+    def __init__(self):
+        """
+
+        """
+        lower_char = 'qwertyuiopasdfghjklzxcvbnm'
+        upper_char = lower_char.upper()
+        nums = '1234567890'
+        symbols = '!@#$%^&*()-=_+[]:;",.<>/?`~'
+        invalid_symbols = str()
+        answer = list()
+        seed = int()
+        password = str()
+        return
+
+    def read_file_as_bytes(self):
+        """
+
+        """
+        return
+    
+    def write_file_from_bytes(self):
+        """
+
+        """
+        return
+
+    def ask_question(self):
+        """
+
+        """
+        return
+
+    def generate_seed(self):
+        """
+
+        """
+        return
+
+    def scramble(self):
+        """
+
+        """
+        return
+
+    def substitute(self):
+        """
+
+        """
+        return
+
+if __name__ == "__main__":
+    
+    pass
+    
